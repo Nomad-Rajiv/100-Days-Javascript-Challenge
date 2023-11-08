bd087c4e0ff867397f5bf02848cbe8e753a9dc2f
Rajiv <rajivkum2021@gmail.com>
2023-11-08 14:04:41 +0530

    code added

---
+++ swapTwoNum.js
@@ -0,0 +1,14 @@
+let a = 5;
+let b = 10;
+ 
+console.log("Before Swaping");
+console.log("a =", a);
+console.log("b =", b);
+
+console.log("After Swaping");
+let temp = a;
+a = b;
+b = temp;
+
+console.log("a = ", a);
+console.log("b = ", b);
