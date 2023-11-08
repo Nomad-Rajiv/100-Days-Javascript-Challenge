48ef562e976696e1a099f7ae67803d701a9b8070
Rajiv <rajivkum2021@gmail.com>
2023-11-08 13:11:54 +0530

    first code added

--- first.js
+++ first.js
@@ -1,4 +1,7 @@
-wqhoieqhcbcniobcqi
-scnosqncqhihcie
-cojqncbqoihiq
-cnoiqncoinhqi
+let num = 10;
+if (num % 2 === 0){
+    console.log("The given number " + num + "  is EVEN");
+}
+else {
+    console.log("The number " + num + "  is ODD");
+}
