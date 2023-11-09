89c70d0818407742b608636f64a1ff7da2cb2a50
Rajiv <rajivkum2021@gmail.com>
2023-11-09 11:29:24 +0530

    Day 3

---
+++ GReatestOfThreeNum.js
@@ -0,0 +1,14 @@
+let a = 10;
+let b = 20;
+let c = 30;
+
+
+if( a > b && a > c){
+    console.log("A is greatest number ", a);
+}
+else if(b > a && b >c){
+    console.log("B is greatest number ", b);
+}
+else {
+    console.log("C is greatest number", c);
+}
