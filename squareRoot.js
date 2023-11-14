37656ef8df659f74c9b7e0e93a54ed7bbb320328
Rajiv <rajivkum2021@gmail.com>
2023-11-14 12:39:23 +0530

    square root code added

---
+++ squareRoot.js
@@ -0,0 +1,6 @@
+function findSquareRoot(number) {
+    return Math.sqrt(number);
+}
+const inputNumber = 100;
+const result = findSquareRoot(inputNumber);
+console.log(`Square root of ${inputNumber} is ${result}`);
