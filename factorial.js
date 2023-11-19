15184cab418d932fb136a2d50eb4fde88803dd4c
Rajiv <rajivkum2021@gmail.com>
2023-11-19 20:11:33 +0530

    factorial code add

---
+++ factorial.js
@@ -0,0 +1,10 @@
+function factorial(n){
+  if(n < 0){
+    return 'Factorial of negative number is not possible';
+  }
+  else if ( n === 0 || n === 1){
+    return 1;
+  }
+  return n * factorial( n- 1);
+}
+console.log(factorial(1));
