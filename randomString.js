3890f3a0f97a8b12b4985f8943f77f625f64a6a1
Rajiv <rajivkum2021@gmail.com>
2023-12-19 22:53:57 +0530

    third code added

---
+++ randomString.js
@@ -0,0 +1,13 @@
+// JavaScript Program to Generate Random String
+
+
+function createRandomString(string_length)  {
+  var random_string = "";
+  var charecter = "ABCDEFGHIJKLMNOPQRESTUVWXYZ0123456789abcdefghijklmnopqrestuvwxyz";
+  for(var i = 0; i < string_length; i++){
+    random_string += charecter.charAt(Math.floor(Math.random() * charecter.length))
+  }
+  return random_string
+}
+
+console.log(createRandomString(5))
